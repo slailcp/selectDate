b35a36e2e07d35994919484a79fb8e0b2032e0f9
slailcp <31639153+slailcp@users.noreply.github.com>
2018-04-25 15:43:43 +0800

    Update selectdate.vue

--- travelNew/src/components/SelectDate/selectdate.vue
+++ travelNew/src/components/SelectDate/selectdate.vue
@@ -1,155 +1,119 @@
 <template>
-    <div>
-      <div class="select-date">
-        <div v-for="item in pullDate">
-          <div class="date-title">{{item.title}}</div>
-          <ul>
-            <li v-for="day in item.date" :class="day.classname" @click="(day.classname==='day'? dayClickEvent(day): null)">
-              {{day.day}}
-              <i v-for="tag in day.tags" :class="tag[0]">
-                {{tag[1]}}<span v-show="tag[0]==='price'">元</span>
-              </i>
-            </li>
-          </ul>
-        </div>
+  <div>
+    <div class="select-date">
+      <ul>
+        <li>一</li>
+        <li>二</li>
+        <li>三</li>
+        <li>四</li>
+        <li>五</li>
+        <li>六</li>
+        <li>七</li>
+      </ul>
+      <div v-for="item in pullDate">
+        <div class="date-title">{{item.title}}</div>
+        <ul>
+          <li v-for="day in item.date" :class="day.classname"
+              @click="(day.classname==='day'? dayClickEvent(day): null)">
+            {{day.day}}
+            <i v-for="tag in day.tags" :class="tag[0]">
+              {{tag[1]}}<span v-show="tag[0]==='price'">元</span>
+            </i>
+          </li>
+        </ul>
       </div>
     </div>
+
+  </div>
 </template>
 
 <script>
-export default {
-  props: {
-    modal: {
-      type: Object
-    }
-  },
-  data() {
-    return {
-      startDate: this.modal.startDate,
-      endDate: this.modal.endDate,
-      dateJson: this.modal.dateJson
-    }
-  },
-  computed: {
-      pullDate() {
-        var dateObj = {};
-        let allDate = this.getAll(this.startDate, this.endDate);
-        allDate.forEach((item, index) => {
-          if (index > 0) { // 当月一号以后
-            // 前一天和后一天的月份是否相当？那么就是同一个月的：那么就是下个月的(如果超过当前的月份就是下个月一号)
-            item.month === allDate[index - 1].month ? this.dateObjPushData(item, dateObj, false) : this.dateObjPushData(item, dateObj, true);
-          } else { // 当月一号
-            this.dateObjPushData(item, dateObj, true);
-          }
-        })
-        return dateObj;
-      }
-  },
-  methods: {
-    dayClickEvent(data) {
-      this.modal.dateIsShow = !this.modal.dateIsShow;
-      let date = data.month + '月' +data.day +'日'
-      this.modal.dateText = date;
-    },
-    dateObjPushData(item, dateObj, isWeek) {
-      let yearMonth = item.year + '年' + item.month+ '月';
-      let yearMonthDay = item.year + '-' + item.month + '-' + item.day;
-      let className = '';
-      let key = Date.parse(new Date(item.year + '-' + item.month + '-' + 1));
-      let tag = this.pushTag(yearMonthDay);
-      switch (this.compareDate(this.startDate, this.endDate, yearMonthDay)) {
-        case 'day':
-          className = 'day';
-          break;
-        case 'future':
-          className = 'future';
-          break;
-        case 'pass':
-          className = 'pass';
-          break;
-      };
-      item.classname = className;
-      item.tags = tag;
-       if (isWeek) { // 每个月的一号
-          console.log(item)
-          let week = item.week; // 周几
-          if (!dateObj[key]) { // 如果没有存储当前月的标识 (以月份为单位进行存储)
-            dateObj[key] = {}; // 就创建一个对象标识
-            for (let i = 0; i < week; i++) { // 对本月一号之前的周几补全。
-              !dateObj[key].date ? dateObj[key].date = [] : dateObj[key].date.push({}); // 如果当前月份没有存储当前天数用的数组,就创建一个空数组，如果有，就向里面添加一个空对象; (空对象是用来占位置的，用来填充月份前面的空白)
-            }
-            dateObj[key].title = yearMonth;
-            !dateObj[key].date ? dateObj[key].date = [item] : dateObj[key].date.push(item); // 如果当前月份没有存储当前天数用的数组,就创建一个空数组,并且初始化存放item,如果有数组，就向里面依次填充
-          }
-        } else {
-//          if (!dateObj[key]) { //
-//            console.log(dateObj[key]);
-//            dateObj[key] = {};
-//            !dateObj[key].date ? dateObj[key].date = [] : dateObj[key].date.push(item);
-//          } else {
-//            !dateObj[key].date ? dateObj[key].date = [] : dateObj[key].date.push(item);
-//          }
-          dateObj[key].date.push(item)
-        }
-    },
-    pushTag(yearMonthDay) { // 添加 价格信息，是否休息等。。。
-      let tags = [];
-      for (let i = 0; i < this.dateJson.length; i++) {
-        if (yearMonthDay === this.dateJson[i].date) {
-          for (let key in this.dateJson[i]) {
-            tags.push([key, this.dateJson[i][key]]);
-          }
-          break;
-        }
-      }
-      return tags;
-    },
-    compareDate(date1, date2, now) { // 当前日期是否在日期区间
-      let d1 = new Date(date1);
-      let d2 = new Date(date2);
-      let n = new Date(now);
-      if (date1 !== '' && now !== '' && d1 > n) {
-        return 'pass';
-      }
-      if (date2 !== ''&& now !== '' && n > d2) {
-        return 'future';
-      }
-      return 'day';
-    },
-    getFirstAndLastMonthDay(year, month) { // 获取当前月的最后一天
-       let day = new Date(year, month, 0);
-       let lastdate = year + '-' + month + '-' + day.getDate(); // 获取当月最后一天日期
-       return lastdate;
-    },
-    DateFormat(obj) {
-          let mouth = (obj.getMonth() + 1)>=10?(obj.getMonth() + 1):((obj.getMonth() + 1));
-          let day = obj.getDate()>=10?obj.getDate():(obj.getDate());
-          let week = new Date(obj.getFullYear() + '/' + mouth + '/' + day).getDay()
-          return ({year: obj.getFullYear(), month: mouth, day: day, week: week}); // 返回日期。
-    },
-    getAll(begin, end) { // 获取两个日期间的所有日期
-      let myBegin = begin.split('-')[0] + '-' + begin.split('-')[1] + '-' + 1;
-      let myEnd = this.getFirstAndLastMonthDay(end.split('-')[0], end.split('-')[1]);
-      let dateArr = [];
-      let ab = myBegin.split('-');
-      let ae = myEnd.split('-');
-      let db = new Date();
-      db.setUTCFullYear(ab[0], ab[1] - 1, ab[2]);
-      let de = new Date();
-      de.setUTCFullYear(ae[0], ae[1] - 1, ae[2]);
-      let unixDb = db.getTime();
-      let unixDe = de.getTime();
-      for (let k = unixDb; k <= unixDe;) {
-          dateArr.push(this.DateFormat(new Date(parseInt(k))));
-          k = k + 24 * 60 * 60 * 1000;
-      }
-      return dateArr;
-    }
-  },
-  created() {
+  import moment from 'moment';
 
+  export default {
+    props: ['modal'],
+    data() {
+      return {
+        startDate: this.modal.startDate,
+        endDate: this.modal.endDate,
+        dateJson: this.modal.dateJson
+      }
+    },
+    computed: {
+      pullDate() {
+        return this.getAll(this.startDate, this.endDate)
+      }
+    },
+    created() {
+      // console.log(this.getAll('2017-08-05','2018-08-09'))
+    },
+    methods: {
+      dayClickEvent(data) {
+        this.modal.dateIsShow = !this.modal.dateIsShow;
+        let date = data.month + '月' + data.day + '日'
+        this.modal.dateText = date;
+      },
+      pushTag(yearMonthDay) { // 添加 价格信息，是否休息等。。。
+        let tags = [];
+        for (let i = 0; i < this.dateJson.length; i++) {
+          if (moment(yearMonthDay).format('x') === moment(this.dateJson[i].date).format('x')) {
+            for (let key in this.dateJson[i]) {
+              tags.push([key, this.dateJson[i][key]]);
+            }
+            break;
+          }
+        }
+        return tags;
+      },
+      getAll(start, end) { // 获取两个日期间的所有日期数据
+        const sd = Number(moment(start).startOf('month').format('x')); // 本月第一天
+        const ed = Number(moment(end).endOf('month').format('x')); // 本月最后一天
+
+        let dataObject = {};
+
+        dataObject[sd] = {title: moment(start).format('YYYY年MM月'), date: []} // 初始第一个月
+        for (let w = 1; w < moment(sd).weekday(); w++) { // 对本月一号之前的周几补全。
+          dataObject[sd].date.push({year: '', month: '', day: '', week: w});// 如果当前月份没有存储当前天数用的数组,就创建一个空数组，如果有，就向里面添加一个空对象; (空对象是用来占位置的，用来填充月份前面的空白)
+        }
+
+        for (let i = sd; i <= ed;) {
+          const firstDay = Number(moment(i).startOf('month').format('x')); // 当月第一天;--作为每个月的唯一标示
+
+          if (moment(i).format('x') === moment(moment(i).startOf('month').format('YYYY-MM-DD')).format('x') && i !== sd) { // 如果是当月的第一天,添加下个月的数据
+            console.log(i, sd)
+            const op = {
+              title: moment(i).add(1, 'days').format('YYYY年MM月'), // 下个月的第一天
+              date: []
+            }
+            for (let w = 1; w < moment(i).weekday(); w++) { // 对本月一号之前的周几补全。
+              op.date.push({year: '', month: '', day: '', week: w});// 如果当前月份没有存储当前天数用的数组,就创建一个空数组，如果有，就向里面添加一个空对象; (空对象是用来占位置的，用来填充月份前面的空白)
+            }
+            dataObject[i] = op;
+          }
+
+          let className = '';
+          if (i >= moment(start).format('x') && i <= moment(end)) { // 是否在开始和结束之间
+            className = 'day';
+          } else {
+            className = 'pass';
+          }
+
+          const tag = this.pushTag(moment(i).format('YYYY-MM-DD')); // 折扣，休息等信息
+          const option = {
+            year: moment(i).format('YYYY'),
+            month: moment(i).format('MM'),
+            day: moment(i).format('DD'),
+            week: moment(i).weekday(),
+            classname: className,
+            tags: tag
+          }
+          dataObject[firstDay].date.push(option);
+          i = Number(moment(i).add(1, 'days').format('x')); // 下次赋值
+        }
+        return dataObject;
+      }
+    }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
