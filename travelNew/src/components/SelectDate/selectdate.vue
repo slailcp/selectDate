4b21d1c05c765565d012df26d7ef2fcca0fd75e3
slailcp <31639153+slailcp@users.noreply.github.com>
2018-04-25 15:59:51 +0800

    Update selectdate.vue

--- travelNew/src/components/SelectDate/selectdate.vue
+++ travelNew/src/components/SelectDate/selectdate.vue
@@ -1,179 +1,212 @@
 <template>
-  <div>
-    <div class="select-date">
-      <ul>
-        <li>一</li>
-        <li>二</li>
-        <li>三</li>
-        <li>四</li>
-        <li>五</li>
-        <li>六</li>
-        <li>七</li>
-      </ul>
-      <div v-for="item in pullDate">
-        <div class="date-title">{{item.title}}</div>
-        <ul>
-          <li v-for="day in item.date" :class="day.classname"
-              @click="(day.classname==='day'? dayClickEvent(day): null)">
-            {{day.day}}
-            <i v-for="tag in day.tags" :class="tag[0]">
-              {{tag[1]}}<span v-show="tag[0]==='price'">元</span>
-            </i>
-          </li>
-        </ul>
+  <div class="flight">
+    <div class="to-arrive-city">
+      <div class="to-city" @click="toCity">
+        <div class="citytitle">出发城市</div>
+        <div class="cityname">{{modalData.citytoName}}</div>
+      </div>
+      <div class="change-wrapper">
+        <div class="icon-change"><img src="./roundtrip_icon.png"/></div>
+      </div>
+      <div class="arrive-city" @click="arrive">
+        <div class="citytitle">到达城市</div>
+        <div class="cityname">{{modalData.cityarriveName}}</div>
+      </div>
+      <selectcity v-show="modalData.isShow" :modalData="modalData"></selectcity>
+    </div>
+    <div class="to-date">
+      <div class="left-wrapper" @click="selectDate">
+        <div class="title">出发时间</div>
+        <div class="name">{{modalData.dateText}}</div>
+      </div>
+      <div class="icon-wrapper">
+        <i class="icon-arrow-right"></i>
       </div>
     </div>
-
+    <div class="type">
+      <div class="left-wrapper">
+        <div class="title">出差类型</div>
+        <div class="name">商务出行</div>
+      </div>
+      <i class="icon-wrapper">
+        <i class="icon-arrow-right"></i>
+      </i>
+    </div>
+    <router-link to="/flightList" class="search-btn">立即搜索</router-link>
+    <div class="lately－talk">
+      <span class="talk-title">-最近乘坐-</span>
+      <span class="talk-train">北京－杭州</span>
+      <span class="talk-train">北京－杭州</span>
+      <span class="talk-train">北京－杭州</span>
+      <span class="talk-train">北京－杭州</span>
+    </div>
+    <transition name="dateHide">
+      <div class="flight-selectdate" v-show="modalData.dateIsShow">
+        <selectdate :modal="modalData" @selectDate="selectDateEvent"></selectdate>
+      </div>
+    </transition>
   </div>
 </template>
 
 <script>
-  import moment from 'moment';
-
-  export default {
-    props: ['modal'],
-    data() {
-      return {
-        startDate: this.modal.startDate,
-        endDate: this.modal.endDate,
-        dateJson: this.modal.dateJson
-      }
-    },
-    computed: {
-      pullDate() {
-        return this.getAll(this.startDate, this.endDate)
-      }
-    },
-    created() {
-      // console.log(this.getAll('2017-08-05','2018-08-09'))
-    },
-    methods: {
-      dayClickEvent(data) {
-        this.$emit('selectDate',data);
-      },
-      pushTag(yearMonthDay) { // 添加 价格信息，是否休息等。。。
-        let tags = [];
-        for (let i = 0; i < this.dateJson.length; i++) {
-          if (moment(yearMonthDay).format('x') === moment(this.dateJson[i].date).format('x')) {
-            for (let key in this.dateJson[i]) {
-              tags.push([key, this.dateJson[i][key]]);
-            }
-            break;
-          }
-        }
-        return tags;
-      },
-      getAll(start, end) { // 获取两个日期间的所有日期数据
-        const sd = Number(moment(start).startOf('month').format('x')); // 本月第一天
-        const ed = Number(moment(end).endOf('month').format('x')); // 本月最后一天
-
-        let dataObject = {};
-
-        dataObject[sd] = {title: moment(start).format('YYYY年MM月'), date: []} // 初始第一个月
-        for (let w = 1; w < moment(sd).weekday(); w++) { // 对本月一号之前的周几补全。
-          dataObject[sd].date.push({year: '', month: '', day: '', week: w});// 如果当前月份没有存储当前天数用的数组,就创建一个空数组，如果有，就向里面添加一个空对象; (空对象是用来占位置的，用来填充月份前面的空白)
-        }
-
-        for (let i = sd; i <= ed;) {
-          const firstDay = Number(moment(i).startOf('month').format('x')); // 当月第一天;--作为每个月的唯一标示
-
-          if (moment(i).format('x') === moment(moment(i).startOf('month').format('YYYY-MM-DD')).format('x') && i !== sd) { // 如果是当月的第一天,添加下个月的数据
-            console.log(i, sd)
-            const op = {
-              title: moment(i).add(1, 'days').format('YYYY年MM月'), // 下个月的第一天
-              date: []
-            }
-            for (let w = 1; w < moment(i).weekday(); w++) { // 对本月一号之前的周几补全。
-              op.date.push({year: '', month: '', day: '', week: w});// 如果当前月份没有存储当前天数用的数组,就创建一个空数组，如果有，就向里面添加一个空对象; (空对象是用来占位置的，用来填充月份前面的空白)
-            }
-            dataObject[i] = op;
-          }
-
-          let className = '';
-          if (i >= moment(start).format('x') && i <= moment(end)) { // 是否在开始和结束之间
-            className = 'day';
-          } else {
-            className = 'pass';
-          }
-
-          const tag = this.pushTag(moment(i).format('YYYY-MM-DD')); // 折扣，休息等信息
-          const option = {
-            year: moment(i).format('YYYY'),
-            month: moment(i).format('MM'),
-            day: moment(i).format('DD'),
-            week: moment(i).weekday(),
-            classname: className,
-            tags: tag
-          }
-          dataObject[firstDay].date.push(option);
-          i = Number(moment(i).add(1, 'days').format('x')); // 下次赋值
-        }
-        return dataObject;
+import selectcity from '@/components/selectCity/selectcity'
+import selectdate from '@/components/SelectDate/selectdate' // 所有日期全部展示出来
+// import selectdate from '@/components/SelectDate/selectdatearow' // 点击一次展示一个月
+export default {
+  data () {
+    return {
+      modalData: { // 日期上面数据，例如价格，是否打折等等...
+        index: 0,
+        isShow: false,
+        citytoName: '北京',
+        cityarriveName: '杭州',
+        dateIsShow: false,
+        dateText: '6月8日',
+        startDate: '2017-8-10',
+        endDate: '2018-1-15',
+        dateJson: [{
+         date: '2017-8-11',
+         price: '100'
+        },
+        {
+         date: '2017-8-13',
+         price: '100',
+         rest: '休'
+        },
+        {
+         date: '2017-8-15',
+         price: '100',
+         discount: '折'
+        },
+        {
+         date: '2017-8-16',
+         price: '100',
+         rest: '假'
+        }]
       }
     }
+  },
+  components: {
+    selectcity,
+    selectdate
+  },
+  methods: {
+    selectDate() {
+      this.modalData.dateIsShow = !this.modalData.dateIsShow;
+    },
+    selectDateEvent(data) {
+      console.log(data)
+      this.modalData.dateIsShow = !this.modalData.dateIsShow;
+      let date = data.month + '月' + data.day + '日'
+      this.modalData.dateText = date;
+    },
+    toCity(event) {
+      this.modalData.isShow=true;
+      this.modalData.index=0;
+      this.$root.bindEvent.$emit('cityClick', event.target);
+    },
+    arrive() {
+      this.modalData.isShow=true;
+      this.modalData.index=1;
+      // this.$root.bindEvent.$emit('cityClick', event.target);
+    }
   }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="stylus" rel="stylesheet/stylus">
-.select-date
-  background: #fafafa
-  color:#fff
-  .date-title
-    width: 100%
-    background-color: transparent
-    color: #333
-    line-height: 1rem
-    font-weight: 400
-    text-align: center
-    letter-spacing: .025rem
-    font-size:18px
-    z-index: 9
-  ul
-    overflow: hidden
+.flight
+  .to-arrive-city
+    display:flex
+    width:100%
+    padding:.15rem 0
     border-bottom:1px solid #e8e8e8
-    background: #fafafa
-    li
-      position:relative
-      float: left
-      width:52px
-      height:56px
-      margin-bottom:-1px
-      border-bottom:1px solid #e8e8e8
-      text-align:center
-      line-height: 58px
-      font-size:18px
-      &.pass,&.future
-        color:#ccc
-        opacity:0.5
-      &.day
-        color:#333
-      i
+    font-size:0.12rem
+    .to-city,.arrive-city
+      flex:1
+      padding:0 .2rem
+      font-size:.16rem
+      .citytitle
+        padding:.12rem 0
+        color:#999
+      .cityname
+        font-size:0.3rem
+        color:#666
+    .arrive-city
+      text-align:right
+    .change-wrapper
+      flex:0 0 0.65rem
+      width: 0.65rem
+      font-size:0
+      .icon-change
+        display:flex
+        align-items:center
+        width: 0.65rem
+        height: 0.65rem
+        border: 1px solid #292929
+        border-radius: 50%
+        justify-content:center
+        img
+          flex:0 0 0.4rem
+          width:0.4rem
+  .to-date,.type
+    display:flex
+    align-items:center
+    padding:.12rem 0.2rem
+    border-bottom:1px solid #e8e8e8
+    font-size:.16rem
+    .left-wrapper
+      flex:1
+      align-items:center
+      .title
+        padding:.12rem 0
+        color:#999
+      .name
+        font-size:0.3rem
+        color:#666
+    .icon-wrapper
+      flex:0 0 0.24rem
+      width:.24rem
+      height:.24rem
+      .icon-arrow-right
         display:block
-        position:absolute
-        color:#333
-        font-style:normal
-        font-size:12px
-        &.date
-          display:none
-          font-style:none
-        &.rest,&.discount
-          right:0.1rem
-          top:0.1rem
-          width:0.3rem
-          height:0.3rem
-          background:#4AC29A
-          line-height:0.3rem
-          color:#fff
-        &.discount
-          background:#ff6600
-          color:#fff
-        &.price
-          bottom:0
-          left:0
-          width:100%
-          color:#333
-          font-size:12px
-          line-height:0.3rem
-
+        width:.24rem
+        height:.24rem
+        background:url('./black_arrowright_icon.png') no-repeat;
+        background-size:.24rem .24rem
+  .search-btn
+    display:block
+    margin:.2rem
+    padding:.25rem 0
+    background:#ff6600
+    border-radius:.1rem
+    color:#fff
+    text-align:center
+    font-size:.36rem
+  .lately－talk
+    text-align:center
+    font-size:.28rem
+    .talk-title,.talk-train
+      display:block
+      padding:.12rem 0
+      color:#999
+    .talk-train
+      color:#666
+  .flight-selectdate
+    position:fixed
+    top:0
+    bottom:0
+    left:0
+    width:100%
+    overflow:auto
+    z-index:999
+    background:#fafafa
+    transition:all .4s
+    &.dateHide-enter-active,&.dateHide-leave
+      opacity:1
+      transform:translate3d(0,0,0)
+    &.dateHide-leave-active,&.dateHide-enter
+      opacity:0
+      transform:translate3d(-720px,0,0)
 </style>
